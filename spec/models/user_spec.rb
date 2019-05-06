466dce64ce5a335b0cb2ef25304afffa66deb73b
FrozenIce0617 <prosdev087@gmail.com>
2019-05-07 01:05:45 +1000

    RSpec Config

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,5 +1,14 @@
 require 'rails_helper'
+require 'database_cleaner'
 
 RSpec.describe User, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe "creation" do
+    before do
+      @user = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf")
+    end
+    
+    it "can be created" do
+      expect(@user).to be_valid
+    end
+  end
 end
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -38,14 +38,14 @@ RSpec.configure do |config|
   # If you're not using ActiveRecord, or you'd prefer not to run each of your
   # examples within a transaction, remove the following line or assign false
   # instead of true.
-  config.use_transactional_fixtures = false
+  config.use_transactional_fixtures = true
 
   # DatabaseCleaner Configuration
-  config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
-  config.before(:each) { DatabaseCleaner.strategy(:transaction) }
-  config.before(:each, :js => true) { DatabaseCleaner.strategy(:truncation) }
-  config.before(:each) { DatabaseCleaner.start }
-  config.after(:each) { DatabaseCleaner.clean }
+  # config.before(:suite) { DatabaseCleaner.clean_with(:truncation) }
+  # config.before(:each) { DatabaseCleaner.strategy(:transaction) }
+  # config.before(:each, :js => true) { DatabaseCleaner.strategy(:truncation) }
+  # config.before(:each) { DatabaseCleaner.start }
+  # config.after(:each) { DatabaseCleaner.clean }
 
   # RSpec Rails can automatically mix in different behaviours to your tests
   # based on their file location, for example enabling you to call `get` and
@@ -66,4 +66,5 @@ RSpec.configure do |config|
   config.filter_rails_from_backtrace!
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
+  config.include Devise::Test::IntegrationHelpers, type: :system
 end
