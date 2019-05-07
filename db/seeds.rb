a81424018f880bc4e8e37baaff50a056b4fc272d
FrozenIce0617 <prosdev087@gmail.com>
2019-05-07 22:24:50 +1000

    Create Seed file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,8 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+100.times do | post |
+  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+end
+
+puts "100 Posts have been created"
