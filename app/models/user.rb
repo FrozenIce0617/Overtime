3d3a6a848e26cf251f744f9504a9e13937b98d5c
FrozenIce0617 <prosdev087@gmail.com>
2019-05-09 06:19:14 +1000

    Implement User full name method

--- app/models/user.rb
+++ app/models/user.rb
@@ -6,4 +6,8 @@ class User < ApplicationRecord
          :recoverable, :rememberable, :validatable
 
   validates_presence_of :first_name, :last_name
+
+  def full_name
+    last_name.upcase + ", " + first_name.upcase
+  end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -2,11 +2,11 @@ require "rails_helper"
 require "database_cleaner"
 
 RSpec.describe User, type: :model do
-  describe "creation" do
-    before do
-      @user = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
-    end
+  before do
+    @user = User.create(email: "test1@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+  end
 
+  describe "creation" do
     it "can be created" do
       expect(@user).to be_valid
     end
@@ -15,6 +15,12 @@ RSpec.describe User, type: :model do
       @user.first_name = nil
       @user.last_name = nil
       expect(@user).to_not be_valid
+    end
+  end
+
+  describe "custome name methods" do
+    it "has a full name method that combines first and last name" do
+      expect(@user.full_name).to eq("SNOW, JON")
     end
   end
 end
