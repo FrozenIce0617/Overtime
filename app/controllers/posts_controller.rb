05a1b1a53c0ab2514c9f457c779f7551c3301104
FrozenIce0617 <prosdev087@gmail.com>
2019-05-09 05:34:20 +1000

    Update Seed file

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -2,6 +2,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: [:show]
 
   def index
+    @posts = Post.all
   end
 
   def new
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,8 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
-100.times do | post |
-  Post.create!(date: Date.today, rationale: "#{post} rationale content")
+
+@user = User.create(email: "test@test.com", password: "asdfasdf", password_confirmation: "asdfasdf", first_name: "Jon", last_name: "Snow")
+
+puts "1 User created"
+
+100.times do |post|
+  Post.create!(date: Date.today, rationale: "#{post} rationale content", user_id: @user.id)
 end
 
 puts "100 Posts have been created"
