6c86cd75bf0998f2022a2e13e44d44bc92d0c5c5
FrozenIce0617 <prosdev087@gmail.com>
2019-05-10 01:48:55 +1000

    Implement Delete Functionality

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,5 +1,5 @@
 class PostsController < ApplicationController
-  before_action :set_post, only: [:show, :edit, :update]
+  before_action :set_post, only: [:show, :edit, :update, :destroy]
 
   def index
     @posts = Post.all
@@ -32,6 +32,11 @@ class PostsController < ApplicationController
   end
 
   def show
+  end
+
+  def destroy
+    @post.delete
+    redirect_to posts_path, notice: "Your post was deleted successfully"
   end
 
   private
