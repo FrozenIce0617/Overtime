47611e64d0984ee112fdb100d5fa4bea5ec32f48
FrozenIce0617 <prosdev087@gmail.com>
2019-05-10 06:09:42 +1000

    Removed Registration Functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
-  devise_for :users
+  devise_for :users, skip: [:registrations]
   root to: "static#homepage"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
