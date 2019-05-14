1da8b7549eb1330d11143a740c50e910604cf4ba
FrozenIce0617 <prosdev087@gmail.com>
2019-05-15 08:56:13 +1000

    Implemented Enum for status

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
+# frozen_string_literal: true
+
 class Post < ApplicationRecord
+  enum status: { submitted: 0, approved: 1, rejected: 2 }
   belongs_to :user, optional: true
   validates_presence_of :date, :rationale
 end
